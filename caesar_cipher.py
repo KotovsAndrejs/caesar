ebd08153a168df5e804c33723a41290b70590095
KotovsAndrejs <125635760+KotovsAndrejs@users.noreply.github.com>
2023-11-15 13:21:06 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,24 @@
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
+def e(text, rotation): #Функция отвечает за  режим шифрования
+  text = text.upper()
+  newtext = ""
+  for c in text: #Цикл перебирает значения в исходном тексте
+    if (a.find(c) == -1): #Цикл находит
+      newtext += c
     else:
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
+      newtext += (a[(a.find(c) + rotation) % len(a)])
+  return newtext
 
-def d(t, o):
-  r = ""
-  for c in t:
+def d(text, rotation): #Функция отвечает за  режим дешифрования
+  text = text.upper()
+  newtext= ""
+  for c in text: #Цикл перебирает значения в исходном тексте
     if (a.find(c) == -1):
-      r += c
+      newtext += c
     else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+      newtext += (a[(a.find(c) - rotation) % len(a)])
+  return newtext
 
 w = """
 1. Encrypt text
@@ -25,14 +27,14 @@ w = """
 Choose mode: """
 mode = int(input(w))
 
-if mode == 1:
+if mode == 1: #При выборе, код запросит текст, ротацию и выведет зашифрованный текст
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
   print("Encrypted: " + e(text, rotation))
-elif mode == 2:
+elif mode == 2: #При выборе, код запросит текст, ротацию и выведет дешифрованный текст
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
   print("Decrypted: " + d(text, rotation))
-elif mode == 3:
+elif mode == 3: #шутка
   print("Bruteforcing...")
   print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
